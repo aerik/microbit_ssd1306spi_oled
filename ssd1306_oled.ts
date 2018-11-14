9e2d224e4ee0c847ddd5cb13a41b1d19ce6782f2
aerik <asylvan@gmail.com>
2018-11-14 13:43:06 -0800

    consolidate buffer use back into one class, other improvements

--- ssd1306_oled.ts
+++ ssd1306_oled.ts
@@ -1,9 +1,15 @@
 namespace SSD1306oled {
 
     const screenSize = 1024;
+    //rowCount * colCount must equal screenSize
+    const rowCount = 64;
+    const colCount = 128;
 
     //private singleton class
     class SSD1306oledInstance {
+
+        private pageCount: number;
+        private screenBuffer: Buffer; //each char is (at least) a byte
 
         private _cmd(cAry: number[]): void {
             pins.P16.digitalWrite(false);
@@ -22,6 +28,7 @@ namespace SSD1306oled {
         }
 
         constructor() {
+            this.pageCount = rowCount / 8;
             let x = '\xAE\xA4\xD5\xF0\xA8\x3F\xD3\x00\x00\x8D\x14\x20\x00\x21\x00\x7F\x22\x00\x3F\xa1\xc8\xDA\x12\x81\xCF\xd9\xF1\xDB\x40\xA6\xd6\x00\xaf';
             pins.P15.digitalWrite(false);
             pins.spiPins(DigitalPin.P14, DigitalPin.P14, DigitalPin.P13);
@@ -30,6 +37,13 @@ namespace SSD1306oled {
             pins.P15.digitalWrite(true);
             this._cmdTxt(x);
             this.clear_screen();
+        }
+
+        public use_buffer() {
+            if (this.screenBuffer == null) {
+                this.drawTextAt("call drawBuffer", 0, 1);
+                this.screenBuffer = pins.createBuffer(screenSize);
+            }
         }
 
         protected _set_pos(col: number, page: number) {
@@ -43,49 +57,79 @@ namespace SSD1306oled {
             for (let i = 0; i < screenSize; i++) {
                 pins.spiWrite(0);
             }
+            if (this.screenBuffer != null) this.screenBuffer.fill(0);
         }
 
         public drawValueAt(val: number, col: number, page: number) {
-            this._set_pos(col, page);
-            pins.spiWrite(val);
+            if (col < 0) col = 0;
+            if (col >= colCount) {
+                col = colCount - 1;
+            }
+            if (page < 0) page = 0;
+            if (page >= this.pageCount) page = this.pageCount - 1;
+            if (this.screenBuffer == null) {
+                this._set_pos(col, page);
+                pins.spiWrite(val);
+            } else {
+                let offset = colCount * page + col;
+                this.screenBuffer.setNumber(NumberFormat.Int8LE, offset, val)
+            }
         }
-    }
-
-    //private singleton class
-    class SSD1306oledBufferedInstance extends SSD1306oledInstance {
-
-        private screenBuffer: Buffer; //each char is (at least) a byte
-
-        constructor() {
-            super();
-            this.screenBuffer = pins.createBuffer(1024);
+        public drawTextAt(txt: string, col: number, page: number) {
+            if (col < 0) col = 0;
+            if (col >= colCount) {
+                col = colCount - 1;
+            }
+            if (page < 0) page = 0;
+            if (page >= this.pageCount) page = this.pageCount - 1;
+            let cVals = vFont.GetCharacterColValues(txt);
+            if (this.screenBuffer == null) {
+                this._set_pos(col, page);
+                for (let i = 0; i < cVals.length; i++) {
+                    pins.spiWrite(cVals[i]);
+                }
+            } else {
+                let offset = colCount * page + col;
+                for (let i = 0; i < cVals.length; i++) {
+                    this.screenBuffer.setNumber(NumberFormat.Int8LE, offset + i, cVals[i]);
+                }
+            }
         }
-
-        protected _clear_buffer() {
-            this.screenBuffer.fill(0);
-        }
-
         public draw_screen() {
-            this._set_pos(0, 0);
-            for (let i = 0; i < this.screenBuffer.length; i++) {
-                pins.spiWrite(this.screenBuffer.getNumber(NumberFormat.Int8LE, i));
+            if (this.screenBuffer != null) {
+                this._set_pos(0, 0);
+                for (let i = 0; i < this.screenBuffer.length; i++) {
+                    pins.spiWrite(this.screenBuffer.getNumber(NumberFormat.Int8LE, i));
+                }
+            }
+        }
+        public set_pixel(x: number, y: number) {
+            if (this.screenBuffer == null) {
+                this.clear_screen();
+                this.drawTextAt("Need Buffer", 5, 1)
             }
         }
     }
 
 
+
     //private singleton instance
     //% fixedInstance whenUsed
-    let oled = new SSD1306oledInstance();
+    let oled: SSD1306oledInstance = new SSD1306oledInstance();
 
     export function drawValueAt(val: number, col: number, page: number) {
         oled.drawValueAt(val, col, page);
     }
 
     export function drawTextAt(txt: string, col: number, page: number) {
-        let cVals = vFont.GetCharacterColValues(txt);
-        for (let i = 0; i < cVals.length; i++) {
-            oled.drawValueAt(cVals[i], col + i, page);
-        }
+        oled.drawTextAt(txt, col, page);
+    }
+
+    export function useBuffer() {
+        oled.use_buffer();
+    }
+
+    export function drawBuffer() {
+        oled.draw_screen();
     }
 }
